ed90f6bf3abc2972242c55e633cc5011a9ced6ec
Florian Fay <florian-fay@web.de>
2016-11-28 00:08:11 +0100

    Bootstrap in index.php eingebunden + jquery-Bibliothek hinzugefügt

--- FotoprojektSemester5/index.php
+++ FotoprojektSemester5/index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-</head>
-<body>
-	<?php
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap-4.0.0-alpha.5/dist/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  </head>
+  <body>
+   <?php
 		echo "<h1>Liste Entwickler mit Zugriff auf das Projekt</h1><br>";
 		echo "<ul><li>Kai Kleefisch</li></ul>";
 		echo "<ul><li>Florian Fay</li></ul>";
@@ -12,6 +18,9 @@
 		echo "<ul><li>Kai Bücking</li></ul>";
 	?>
 
-</body>
-
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="bootstrap-4.0.0-alpha.5/dist/js/jquery-3.1.1.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="bootstrap-4.0.0-alpha.5/dist/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+  </body>
 </html>
