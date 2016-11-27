69e0bf0876f408a2c709d0a57950c5164affe2c7
Florian Fay <florian-fay@web.de>
2016-11-28 00:21:19 +0100

    Erstes Beispiel für den Einsatz von Bootsrap eingefügt

--- FotoprojektSemester5/index.php
+++ FotoprojektSemester5/index.php
@@ -10,14 +10,36 @@
     <link rel="stylesheet" href="bootstrap-4.0.0-alpha.5/dist/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
   </head>
   <body>
-   <?php
-		echo "<h1>Liste Entwickler mit Zugriff auf das Projekt</h1><br>";
-		echo "<ul><li>Kai Kleefisch</li></ul>";
-		echo "<ul><li>Florian Fay</li></ul>";
-		echo "<ul><li>Alex</li></ul>";
-		echo "<ul><li>Kai Bücking</li></ul>";
-	?>
-
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+            
+             <?php
+            echo "<h1>Liste Entwickler mit Zugriff auf das Projekt</h1><br>";
+            echo "<ul><li>Kai Kleefisch</li></ul>";
+            echo "<ul><li>Florian Fay</li></ul>";
+            echo "<ul><li>Alex</li></ul>";
+            echo "<ul><li>Kai Bücking</li></ul>";
+        ?>
+            
+        </div>
+        
+         <div class="col-md-6">
+            
+             <?php
+            echo "<h1>Und hier die weiteren Entwickler</h1><br>";
+            echo "<ul><li>Kai Kleefisch</li></ul>";
+            echo "<ul><li>Florian Fay</li></ul>";
+            echo "<ul><li>Alex</li></ul>";
+            echo "<ul><li>Kai Bücking</li></ul>";
+        ?>
+            
+        </div>
+        
+        
+      </div>
+      
+    </div>    
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="bootstrap-4.0.0-alpha.5/dist/js/jquery-3.1.1.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
