9883275feff732062e3bca02d0646b9747677340
Florian Fay <florian-fay@web.de>
2016-11-27 23:35:45 +0100

    Dateien für Bootstrap hinzugefügt

--- FotoprojektSemester5/index.php
+++ FotoprojektSemester5/index.php
@@ -2,14 +2,18 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<script type="text/javascript" src="javaScriptFiles/FirstJavaScriptFile.js"></script>
 </head>
 <body>
 	<?php
 		echo "<h1>Liste Entwickler mit Zugriff auf das Projekt</h1><br>";
 		echo "<ul><li>Kai Kleefisch</li></ul>";
-		echo "<ul><li>Florian Fay</li></lu>";
+		echo "<ul><li>Florian Fay</li></ul>";
+		echo "<ul><li>Alex</li></ul>";
+		echo "<ul><li>Kai Bücking</li></ul>";
 	?>
-
+<!--  	<a href="javascript:ausgabeHalloWelt()">Sag Hallo!</a>
+-->
 </body>
 
 </html>
---
+++ FotoprojektSemester5/javaScriptFiles/FirstJavaScriptFile.js
@@ -0,0 +1,29 @@
+/**
+ * Mehrere Kommentare werden so eingeführt
+ */
+
+
+// Javascript ist Case-Sensitiv
+// Nach jeder Anweisung muss ein Semikolon stehen
+
+// Javascript besitzt vier Datentypen Number, String, Boolean und Objekt
+
+// Javascript ist eine lose typisierte Sprache
+// Wenn kein Wert zugewiesen wird, dann besitzt die Variable den Wert undefined
+var name;
+
+// Ein Block wird mit geschweiften Klammern erstellt
+{
+	name = "";
+	
+	if(isNaN(name)) {
+		// Das ist eine Funktion
+		alert("Name ist keine Zahl");
+	}
+	
+}
+
+function ausgabeHalloWelt() {
+	alert("Hallo Welt");
+}
+
