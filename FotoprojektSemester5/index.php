9ab1a49b3ea4443e12f610a81f7d34af708ca903
Severin Klug <Severin.Klug@gmx.de>
2016-11-28 14:57:41 +0100

    Lit item hinzugefügt

--- FotoprojektSemester5/index.php
+++ FotoprojektSemester5/index.php
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Futureflo/FotoprojektSemester5.git
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -20,6 +21,7 @@
             echo "<ul><li>Florian Fay</li></ul>";
             echo "<ul><li>Alex</li></ul>";
             echo "<ul><li>Kai Bücking</li></ul>";
+            echo "<ul><li>Severin Klug</li></ul>";
             echo "<ul><li>Tim Deisser</li></ul>";
             echo "<ul><li>Melanie Müller</li></ul>";
 			echo "<ul><li>David Fankhänel</li></ul>";
@@ -37,6 +39,7 @@
             echo "<ul><li>Florian Fay</li></ul>";
             echo "<ul><li>Alex</li></ul>";
             echo "<ul><li>Kai Bücking</li></ul>";
+            echo "<ul><li>Severin Klug</li></ul>";
             echo "<ul><li>Tim Deisser</li></ul>";
             echo "<ul><li>Melanie Müller</li></ul>";
 			echo "<ul><li>David Fankhänel</li></ul>";
@@ -54,5 +57,59 @@
     <script src="bootstrap-4.0.0-alpha.5/dist/js/jquery-3.1.1.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
     <script src="bootstrap-4.0.0-alpha.5/dist/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+=======
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap-4.0.0-alpha.5/dist/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+            
+             <?php
+            echo "<h1>Liste Entwickler mit Zugriff auf das Projekt</h1><br>";
+            echo "<ul><li>Kai Kleefisch</li></ul>";
+            echo "<ul><li>Florian Fay</li></ul>";
+            echo "<ul><li>Alex</li></ul>";
+            echo "<ul><li>Kai Bücking</li></ul>";
+            echo "<ul><li>Severin Klug</li></ul>";
+            echo "<ul><li>Tim Deisser</li></ul>";
+            echo "<ul><li>Melanie Müller</li></ul>";
+        ?>
+            
+        </div>
+        
+         <div class="col-md-6">
+            
+             <?php
+            echo "<h1>Und hier die weiteren Entwickler</h1><br>";
+            echo "<ul><li>Kai Kleefisch</li></ul>";
+            echo "<ul><li>Florian Fay</li></ul>";
+            echo "<ul><li>Alex</li></ul>";
+            echo "<ul><li>Kai Bücking</li></ul>";
+            echo "<ul><li>Severin Klug</li></ul>";
+            echo "<ul><li>Tim Deisser</li></ul>";
+            echo "<ul><li>Melanie Müller</li></ul>";
+        ?>
+            
+        </div>
+        
+        
+      </div>
+      
+    </div>    
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="bootstrap-4.0.0-alpha.5/dist/js/jquery-3.1.1.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="bootstrap-4.0.0-alpha.5/dist/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+>>>>>>> 108e6f6 List-Item hinzugefügt
   </body>
 </html>
