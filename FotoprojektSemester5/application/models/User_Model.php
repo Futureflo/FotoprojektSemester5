0650efb2000321b727e8d52a9afdba5ab659d703
alex <alex-finkbeiner@gmx.de>
2016-11-29 22:28:40 +0100

    Add some Database access in user_model for loginsystem

--- FotoprojektSemester5/application/models/User_Model.php
+++ FotoprojektSemester5/application/models/User_Model.php
@@ -4,11 +4,32 @@
       Public function __construct() { 
          parent::__construct(); 
       } 
-      
+            
       Public function getAllUsers(){
       	$query = $this->db->get("user");
       	return $query->result();
       }
 		
+      function get_user($email, $pwd)
+      {
+      	$this->db->where('user_email', $email);
+      	$this->db->where('user_password', md5($pwd));
+      	$query = $this->db->get('user');
+      	return $query->result();
+      }
+      
+      // get user
+      function get_user_by_id($id)
+      {
+      	$this->db->where('id', $id);
+      	$query = $this->db->get('user');
+      	return $query->result();
+      }
+      
+      // insert
+      function insert_user($data)
+      {
+      	return $this->db->insert('user', $data);
+      }
    } 
 ?>
