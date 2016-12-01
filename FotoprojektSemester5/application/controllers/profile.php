4c492faa83ef13c61c1898bed381fa9ef3ae8677
alex <alex-finkbeiner@gmx.de>
2016-12-01 15:59:43 +0100

    Hash Helper Login

---
+++ FotoprojektSemester5/application/controllers/profile.php
@@ -0,0 +1,21 @@
+<?php
+class profile extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('html'));
+		$this->load->database();
+		$this->load->model('user_model');
+	}
+	
+	function index()
+	{
+		$details = $this->user_model->get_user_by_id($this->session->userdata('user_id'));
+		
+	
+		$data['user_name'] = $details[0]->user_firstname . " " . $details[0]->user_name;
+		$data['user_email'] = $details[0]->user_email;
+		$this->load->view('user/profile_view', $data);
+	}
+}
