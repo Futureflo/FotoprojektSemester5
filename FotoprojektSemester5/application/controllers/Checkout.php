31a6927de2881f656c9c4a2275447459361d54aa
Severin Klug <Severin.Klug@gmx.de>
2016-12-02 13:19:25 +0100

    Methoden organisiert.

--- FotoprojektSemester5/application/controllers/Checkout.php
+++ FotoprojektSemester5/application/controllers/Checkout.php
@@ -1,6 +1,10 @@
 <?php
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Checkout extends CI_Controller {
+	// 	https://www.tutorialspoint.com/codeigniter/codeigniter_tempdata.htm
+	// 'item' will be erased after 300 seconds(5 minutes)
+	// 	$this->session->mark_as_temp('item',300);
+	
 	public function index() {
 		$this->load->template ( 'checkout/checkout_view' );
 	}
--- FotoprojektSemester5/application/controllers/Picture.php
+++ FotoprojektSemester5/application/controllers/Picture.php
@@ -1,11 +1,7 @@
 <?php
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Picture extends CI_Controller {
-	// 	https://www.tutorialspoint.com/codeigniter/codeigniter_tempdata.htm
-	// 'item' will be erased after 300 seconds(5 minutes)
-	// 	$this->session->mark_as_temp('item',300);
 
-	
 	public function index() {
 		$this->load->template ( 'errors/404' );
 	}
