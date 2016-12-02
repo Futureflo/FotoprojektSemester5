6bc4fba93332b3a443b2e205c43275a842247b21
Simone Gregg <Simone Gregg@141.31.77.231>
2016-12-02 13:10:09 +0100

    Warenkorb erster Entwurf

--- FotoprojektSemester5/application/views/checkout/checkout_view.php
+++ FotoprojektSemester5/application/views/checkout/checkout_view.php
@@ -1,13 +1,58 @@
 
-<section class="jumbotron text-xs-center">
-	<div class="container">
-		<h1 class="jumbotron-heading">Checkout</h1>
-		<p class="lead text-muted">Something short and leading about the
-			collection below—its contents, the creator, etc. Make it short and
-			sweet, but not too short so folks don't simply skip over it entirely.</p>
-		<p>
-			<a href="#" class="btn btn-primary">Main call to action</a> <a
-				href="#" class="btn btn-secondary">Secondary action</a>
-		</p>
-	</div>
-</section>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<div class="container">
+	<table id="cart" class="table table-hover table-condensed">
+    				<thead>
+						<tr>
+							<th style="width:50%">Product</th>
+							<th style="width:10%">Price</th>
+							<th style="width:8%">Quantity</th>
+							<th style="width:22%" class="text-center">Subtotal</th>
+							<th style="width:10%"></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td data-th="Product">
+								<div class="row">
+									<div class="col-sm-3 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+									<div class="col-sm-9">
+										<h4 class="nomargin">Bild 1</h4>
+										<p>Lorem Ipsum</p>
+									</div>
+								</div>
+							</td>
+							<td data-th="Price">$1.99</td>
+							<td data-th="Größe">
+								 <div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Größe
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="#">XS</a></li>
+    <li><a href="#">S</a></li>
+    <li><a href="#">M</a></li>
+    <li><a href="#">L</a></li>
+    <li><a href="#">XL</a></li>
+  </ul>
+</div>
+							</td>
+							<td data-th="Subtotal" class="text-center">1.99</td>
+							<td class="actions" data-th="">
+								<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+								<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
+							</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr class="visible-xs">
+							<td class="text-center"><strong>Total 1.99</strong></td>
+						</tr>
+						<tr>
+							<td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+							<td colspan="2" class="hidden-xs"></td>
+							<td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+							<td><a href="#" class="btn btn-success btn-block">Zur Kasse<i class="fa fa-angle-right"></i></a></td>
+						</tr>
+					</tfoot>
+				</table>
+</div>
