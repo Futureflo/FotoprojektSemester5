947315c0d639cc16dd413063329ceb7c967c21ca
Tim <Tim@141.31.68.242>
2016-12-05 10:46:36 +0100

    Admin kann nach Usern suchen

--- FotoprojektSemester5/application/views/admin/users_view.php
+++ FotoprojektSemester5/application/views/admin/users_view.php
@@ -13,37 +13,62 @@
 </section>
 
 <div class="col-md-12">
-<div class="col-sm-10 offset-sm-1 col-md-10 offset-md-1">
-	<h1>Users</h1>
-	<div class="table-responsive">
-		<table class="table table-striped table-bordered sortable">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Nachname</th>
-					<th>Vorname</th>
-					<th>e-Mail</th>
-					<th>Passwort</th>
-					<th>Rolle</th>
-				</tr>
-			</thead>
-			<tbody>
+	<div class="col-sm-10 offset-sm-1 col-md-10 offset-md-1">
+		<h1>Users</h1>
+		<input type="text" id="searchTerm" class="search_box"
+			onkeyup="search()" />
+		<div class="table-responsive">
+			<table id="dataTable"
+				class="table table-striped table-bordered sortable">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Nachname</th>
+						<th>Vorname</th>
+						<th>e-Mail</th>
+						<th>Passwort</th>
+						<th>Rolle</th>
+					</tr>
+				</thead>
+				<tbody>
 			<?php
 			foreach ( $users as $user ) {
-				echo "<tr>";
-				echo "<th>" . $user->user_id . "</th>";
-				echo "<th>" . $user->user_name . "</th>";
-				echo "<th>" . $user->user_firstname . "</th>";
-				echo "<th>" . $user->user_email . "</th>";
-				echo "<th>" . $user->user_password . "</th>";
-				echo "<th>" . $user->usro_name . "</th>";
+				echo "<tr class='searchable'>";
+				echo "<td>" . $user->user_id . "</td>";
+				echo "<td>" . $user->user_name . "</td>";
+				echo "<td>" . $user->user_firstname . "</td>";
+				echo "<td>" . $user->user_email . "</td>";
+				echo "<td>" . $user->user_password . "</td>";
+				echo "<td>" . $user->usro_name . "</td>";
 				echo "<tr>";
 			}
 			?>
 			</tbody>
-		</table>
+			</table>
+		</div>
 	</div>
 </div>
-</div>
 
+<script type="text/javascript">
+    function search() {
+        var input, table, tr, td, i;
+        input = document.getElementById('searchTerm').value;
+        table = document.getElementById('dataTable');
+        tr = table.getElementsByClassName('searchable');
+        
+        // Loop through all table rows, and hide those who don't match the search query
+          for (i = 0; i < tr.length; i++) {
+              for(var x = 0; x < tr[0].getElementsByTagName("td").length; x++){
+                td = tr[i].getElementsByTagName("td")[x].innerHTML;
+                if (td) {
+                  if (td.toLowerCase().includes(input.toLowerCase())) {
+                    tr[i].style.display = "";
+                  } else {
+                    tr[i].style.display = "none";
+                  }
+                }
+            } 
+          }
+    }
+</script>
 <script src="<?php echo base_url();?>js/sorttable.js"></script>
