b764a1ab65c7b3ca09279f861b36ec52f870462d
Simone Gregg <Simone Gregg@141.31.77.231>
2016-12-06 14:41:47 +0100

    responsive in arbeit

--- FotoprojektSemester5/application/views/checkout/checkout_view.php
+++ FotoprojektSemester5/application/views/checkout/checkout_view.php
@@ -11,29 +11,31 @@
 <div style="overflow-x:auto;">
 	<table id="cart" class="table table-hover table-condensed">
     				<thead>
-						<tr>
+						<tr class="hidden-xs-down">
 							<th style="width:50%">Produkt</th>
 							<th style="width:10%">Preis</th>
 							<th style="width:8%">Größe</th>
 							<th style="width:22%" class="text-center">Anzahl</th>
-							<th style="width:10%"></th>
+							<th style="width:10%">Preis</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr>
 							<td data-th="Product">
 								<div class="row">
-									<div class="col-sm-3 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
-									<div class="col-sm-9">
-										<h4 class="nomargin">Bild 1</h4>
-										<p>Veranstaltung</p>
-										<p>Fotograf</p>
+									<div class="col-sm-3">
+									<img src="http://placehold.it/100x100" alt="..." class="img-responsive"/>
+									</div>
+									<div class="col-sm-3">
+										<h4>Bild 1</h4>
+										<p class="hidden-sm-down">Veranstaltung</p>
+										<p class="hidden-sm-down">Fotograf</p>
 									</div>
 								</div>
 							</td>
-							<td data-th="Price">1.99€</td>
+							<td class="col-sm-1" data-th="Price">1.99€</td>
 							<td data-th="Größe">
-								 <div class="dropdown">
+								 <div class="dropdown row">
                         <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Größe
                         <span class="caret"></span></button>
                         <ul class="dropdown-menu">
@@ -46,7 +48,7 @@
                         </div>
                         </td>
    							<td data-th="Anzahl">
-								<input type="number" class="form-control text-center" value="1">
+								<input type="number" class="form-control text-center">
 							</td>
 							<td data-th="Subtotal" class="text-center">1.99€</td>
 							<td class="actions" data-th="">
@@ -57,9 +59,9 @@
 					</tbody>
 					<tfoot>
 						<tr>
-							<td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Weitershoppen</a></td>
-							<td colspan="2" class="hidden-xs"></td>
-							<td class="hidden-xs text-center"><strong>Gesamtpreis 1.99€</strong></td>
+							<td class="hidden-sm-down"><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Weitershoppen</a></td>
+							<td colspan="2"></td>
+							<td class="text-center"><strong>Gesamtpreis 1.99€</strong></td>
 							<td><a href="#" class="btn btn-success btn-block">Zur Kasse<i class="fa fa-angle-right"></i></a></td>
 						</tr>
 					</tfoot>
