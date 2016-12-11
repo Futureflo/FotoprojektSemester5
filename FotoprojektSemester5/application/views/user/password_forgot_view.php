641d1433ff9290b1a61c777ba9c974763fc0507c
Kai Kleefisch <Kai.Kleefisch@gmx.de>
2016-12-11 23:40:52 +0100

    View PasswordForgot hinzugefügt

---
+++ FotoprojektSemester5/application/views/user/password_forgot_view.php
@@ -0,0 +1,62 @@
+<section style="padding-top: 70px">
+<div class="signup-center">
+<div class="container">
+<div class="row">
+<div class="col-md-4 offset-md-4">
+<div class="form-PasswordReset">
+<h4>Passwort vergessen?</h4>
+<p>Per E-Mail können Sie Ihr Passwort einfach zurücksetzen. Nach Eingabe der Daten senden wir Ihnen einen Aktivierungslink.</p>
+<input type="text" class="form-control input-sm chat-input" placeholder="Email-ID" name="user_email"/>
+<span class="text-danger"></span>
+<br>
+
+<div align="right">
+<span class="group-btn">
+<button name="submit" type="submit" class="btn btn-primary btn-md">E-Mail anfordern</button>
+</span>
+</div>
+</div>
+	
+<!-- <legend>Signup</legend>
+	
+<div class="form-group">
+<label for="name">First Name</label>
+<input class="form-control" name="user_firstname" placeholder="Your First Name" type="text"/>
+<span class="text-danger"></span>
+</div>
+
+<div class="form-group">
+<label for="name">Last Name</label>
+<input class="form-control" name="user_name" placeholder="Last Name" type="text"/>
+<span class="text-danger"></span>
+</div>
+
+<div class="form-group">
+<label for="email">Email ID</label>
+<input class="form-control" name="user_email" placeholder="Email-ID" type="text"/>
+<span class="text-danger"></span>
+</div>
+
+<div class="form-group">
+<label for="subject">Password</label>
+<input class="form-control" name="user_password" placeholder="Password" type="password" />
+<span class="text-danger"></span>
+</div>
+
+<div class="form-group">
+<label for="subject">Confirm Password</label>
+<input class="form-control" name="user_cpassword" placeholder="Confirm Password" type="password" />
+<span class="text-danger"></span>
+</div>
+
+<div class="form-group">
+<button name="submit" type="submit" class="btn btn-info">Signup</button>
+<button name="cancel" type="reset" class="btn btn-info">Cancel</button>
+</div>  -->
+
+</div>
+</div>
+
+</div>
+</div>
+</section>
