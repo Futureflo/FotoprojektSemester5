69ebb00a1aa036c4eb6e355a7b477a81ed0ce7e1
Raphael Fehrenbach <wi14271@lehre.dhbw-stuttgart.de>
2016-12-12 11:51:57 +0100

    Anpassung Gross und Kleinschreibung Impressum und AGBs

--- FotoprojektSemester5/application/controllers/Impressum.php
+++ FotoprojektSemester5/application/controllers/Impressum.php
@@ -1,5 +1,7 @@
 <?php
-class impressum extends CI_Controller
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Impressum extends CI_Controller
 {
     public function index()
     {
