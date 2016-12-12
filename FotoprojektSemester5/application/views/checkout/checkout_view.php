e029c4821940af90c0184ab36686aefe8ec8a277
Simone Gregg <Simone Gregg@141.31.73.205>
2016-12-12 11:33:00 +0100

    warenkorb angepasst

--- FotoprojektSemester5/application/views/checkout/checkout_view.php
+++ FotoprojektSemester5/application/views/checkout/checkout_view.php
@@ -10,45 +10,53 @@
 <div class="container">
 
 								<div class="row">
-									<div class="col-sm-3">
+									<div class="col-sm-2 col-xs-6">
 									<img src="http://placehold.it/100x100" alt="..." class="img-responsive"/>
 									</div>
-									<div class="col-sm-3">
-										<h5>Bild 1</h5>
-										<p class="hidden-sm-down">Veranstaltung
-										Fotograf</p>
-										<h6>Einzelpreis: <i id="einzelpreis">1.99</i>€</h6>
+									<div class="col-sm-5 col-xs-6">
+										 <ul style="list-style-type:none">
+										 <li><h6>Bild 1</h6></li>
+										 <li>Veranstaltung</li>
+										 <li>Größe: X</li>
+										 <li>Digital/Analog</li>
+										 <li><h6>Einzelpreis: <i id="einzelpreis">1.99</i>€</h6></li>
+										 </ul> 
 									</div>
-									<div class="col-sm-2">
-									<h5>Größe</h5>
-									<p>
-									<h5>Anzahl</h5></div>
-							 <div class="form-group col-sm-3">
-                <select id="Grösse" class="form-control">
-                    <option>XS</option>
-                    <option>S</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
-                </select>
-                        <p><input type="text" class="form-control" id="anzahl" onclick="preisaktualisieren()"></p>
-								<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-								<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
+									<div class="col-sm-1">
+									<h5>Anzahl:</h5></div>
+							 <div class="form-group col-sm-2">
+                        <p><input type="text" class="form-control" id="anzahl" onchange="preisaktualisieren()"></p>								
 							</div>
 							<div class="col-sm-1">
 							<p></p>
 							<h5>Preis</h5> <i id="aktuellerpreis">1.99€</i>
 							</div>
+							<div class="col-sm-1">
+							<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button></div>
 							</div>
+						<hr />	
 						<div class="row">
-							<div class="col-sm-7 col-s-12"><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Weitershoppen</a>
+						<div class="col-sm-0 col-s-0"></div>
+						<div class="col-sm-2 col-xs-10">
+						<h6>Nettopreis:</h6>
+						<h6>Mehrwertsteuer:</h6>
+						<h6>Versandkosten:</h6>
+						<h5>Gesamtpreis:</h5>
 							</div>
-							<div class="col-sm-2 col-s-12"><strong>Gesamtpreis 1.99€</strong>
+						<div class="col-sm-1 col-xs-2">
+						<h6>1.62€</h6>
+						<h6>0.38€</h6>
+						<h6>1.00€</h6>
+						<h5>2.99€</h5>
+						</div>	
+						</div>	
+						<div class="row">
+							<div class="col-sm-9 col-s-12">
 							</div>
 							<div class="col-sm-3 col-s-12"><a href="#" class="btn btn-success btn-block">Zur Kasse<i class="fa fa-angle-right"></i></a>
 							</div>
 						</div>
-			
+			<hr />
 				
 </div>
 <script>
