30a1aac92b3d3b53274d77578f0995a4d78b783f
Tim <Tim@141.31.68.242>
2016-12-13 16:11:37 +0100

    Startseite Carousel mit Bildern, sowie Suchleiste hinzugefügt

--- FotoprojektSemester5/application/views/start_view.php
+++ FotoprojektSemester5/application/views/start_view.php
@@ -1,16 +1,60 @@
+<section class="section-white" style="padding-top: 15px;">
 
-<section class="jumbotron text-xs-center">
-	<div class="container">
-		<h1 class="jumbotron-heading">Album example</h1>
-		<p class="lead text-muted">Something short and leading about the
-			collection below—its contents, the creator, etc. Make it short and
-			sweet, but not too short so folks don't simply skip over it entirely.</p>
-		<p>
-			<a href="#" class="btn btn-primary">Main call to action</a> <a
-				href="#" class="btn btn-secondary">Secondary action</a>
-		</p>
+	<div id="carousel-example-generic" class="carousel slide float-md-left"
+		data-ride="carousel" >
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-example-generic" data-slide-to="0"
+				class="active"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="carousel-item active">
+				<img
+					src="http://haarentferner.net/wp-content/uploads/2015/01/Frau-Bart.jpg"
+					 class="bild">
+			</div>
+			<div class="carousel-item">
+				<img
+					src="http://www.kleinwalsertal.com/fotos/kleinwalsertal/taeler/wildental/wildental_01.jpg"
+					alt="..."  class="bild">
+				<div class="carousel-caption">
+					<h2>Heading</h2>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img
+					src="http://www.kleinwalsertal.com/fotos/kleinwalsertal/taeler/wildental/wildental_01.jpg"
+					alt="..."  class="bild">
+				<div class="carousel-caption">
+					<h2>Heading</h2>
+				</div>
+			</div>
+		</div>
+
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-example-generic"
+			role="button" data-slide="prev"> <span class="icon-prev"
+			aria-hidden="true"></span> <span class="sr-only">Previous</span>
+		</a> <a class="right carousel-control"
+			href="#carousel-example-generic" role="button" data-slide="next"> <span
+			class="icon-next" aria-hidden="true"></span> <span class="sr-only">Next</span>
+		</a>
 	</div>
 </section>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <input type="text" id="searchTerm" class="form-control"
+			onkeyup="" placeholder="Search for event..." style="margin: 20px 10px"/>
+        </div>
+    </div>
+</div>
+
 
 <div class="album text-muted">
 	<div class="container">
@@ -105,7 +149,25 @@
 </div>
 
 
+<script type="text/javascript">
+	function setPics(){
+		var w = (window.innerWidth/100)*99;
+		var h = (w/100)*30;
 
+		var car = document.getElementById("carousel-example-generic");
+        var img = car.getElementsByClassName("bild");
+        
+        car.style.width = w+"px";
+        car.style.height = h+"px";
+        
+        for(var i = 0; i<img.length; i++){
+            img[i].style.width = w + "px";
+            img[i].style.height = h + "px";
+        }
+	}
+    window.addEventListener('resize', setPics, false);
+    window.addEventListener('load', setPics, false);
+</script>
 
 <svg xmlns="http://www.w3.org/2000/svg" width="356" height="280"
 	viewBox="0 0 356 280" preserveAspectRatio="none"
