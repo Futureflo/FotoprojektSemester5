5ebebb735c18556c302858724639c36130ad7721
daydy <daydy@141.31.78.141>
2016-12-14 12:11:07 +0100

    Kontakt View: Betreff hinzugefügt

--- FotoprojektSemester5/application/views/contact/contact_view.php
+++ FotoprojektSemester5/application/views/contact/contact_view.php
@@ -2,15 +2,26 @@
 	<section class="text-xs-left">
 		<div class="container">
 			<div class="">
-				<h2>Kontaktformular</h2>
+				<h2>Kontaktiere uns:</h2>
 				<br> 
-				<input type="text" class="form-control input-sm chat-input" placeholder="Vor- und Nachname eingeben" name="user_name"/>
+				<input type="text" class="form-control input-sm chat-input" placeholder="Vor- und Nachname*" name="user_name"/>
 				<br />
-				<input type="text" class="form-control input-sm chat-input" placeholder="E-Mail-Adresse eingeben" name="user_email"/>
+				<input type="text" class="form-control input-sm chat-input" placeholder="E-Mail-Adresse*" name="user_email"/>
+				<br />
+				<input type="text" class="form-control input-sm chat-input" placeholder="Telefonnummer" name="user_email"/>
 				<br />
 				<div class="form-group">
-  <!--  <label for="comment">Nachricht:</label>  -->
-  <textarea class="form-control" rows="5" placeholder="Nachricht eingeben" id="nachricht"></textarea>
+			    <select class="form-control" id="betreffSelect">
+			      <option style="color: grey">Betreff wählen*</option>
+			      <option>Rechnung</option>
+			      <option>Bestellung</option>
+			      <option>Account</option>
+			      <option>Sonstiges</option>
+			    </select>
+			  </div>
+				<div class="form-group">
+
+  <textarea class="form-control" rows="5" placeholder="Nachricht eingeben*" id="nachricht"></textarea>
 				</div>
 				<span class="group-btn">    
 		            	<button name="submit" type="submit" class="btn btn-primary btn-md">Absenden</button>
