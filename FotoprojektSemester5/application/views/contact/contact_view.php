c418f2303c7928349c24cb458166215483d04d2a
alex <alex-finkbeiner@gmx.de>
2016-12-22 16:12:47 +0100

    contact us

--- FotoprojektSemester5/application/views/contact/contact_view.php
+++ FotoprojektSemester5/application/views/contact/contact_view.php
@@ -1,17 +1,18 @@
 <section class="jumbotron text-xs-center">
 	<section class="text-xs-left">
 		<div class="container">
+			<?php echo form_open('Contact/');?>
 			<div class="">
 				<h2>Kontaktiere uns:</h2>
 				<br> 
-				<input type="text" class="form-control input-sm chat-input" placeholder="Vor- und Nachname*" name="user_name"/>
+				<input type="text" class="form-control input-sm chat-input" placeholder="Vor- und Nachname*" name="name"/>
 				<br />
-				<input type="text" class="form-control input-sm chat-input" placeholder="E-Mail Adresse*" name="user_email"/>
+				<input type="text" class="form-control input-sm chat-input" placeholder="E-Mail Adresse*" name="email"/>
 				<br />
-				<input type="text" class="form-control input-sm chat-input" placeholder="Telefonnummer" name="user_email"/>
+				<input type="text" class="form-control input-sm chat-input" placeholder="Telefonnummer" name="telNum"/>
 				<br />
 				<div class="form-group">
-			    <select class="form-control" id="betreffSelect">
+			    <select class="form-control" id="subject" name="subject">
 			      <option style="color: grey">Betreff wählen*</option>
 			      <option>Rechnung</option>
 			      <option>Bestellung</option>
@@ -21,8 +22,9 @@
 			  </div>
 				<div class="form-group">
 
-  <textarea class="form-control" rows="5" placeholder="Nachricht eingeben*" id="nachricht"></textarea>
+  <textarea class="form-control" rows="5" placeholder="Nachricht eingeben*" id="message" name="message"></textarea>
 				</div>
+				<div id="fehler_span" class="text-danger"><?php  echo $this->session->flashdata('contactMsg'); ?></div>
 				<span class="group-btn">    
 		            	<button name="submit" type="submit" class="btn btn-primary btn-md">Absenden</button>
 		        </span>
