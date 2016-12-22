864a44fb5cfb9e5806be80eb4c44a790e13d4c25
Tim <Tim@141.31.68.242>
2016-12-22 15:56:49 +0100

    Private Events werden mit Schloss angezeigt

--- FotoprojektSemester5/application/views/start_view.php
+++ FotoprojektSemester5/application/views/start_view.php
@@ -1,11 +1,9 @@
 <section class="section-white" style="margin: 3.25rem 0rem 0rem 0rem;">
 
-	<div id="carousel-example-generic" class="carousel slide float-md-left"
-		data-ride="carousel">
+	<div id="carousel-example-generic" class="carousel slide float-md-left" data-ride="carousel">
 		<!-- Indicators -->
 		<ol class="carousel-indicators">
-			<li data-target="#carousel-example-generic" data-slide-to="0"
-				class="active"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
 			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
 		</ol>
@@ -38,12 +36,10 @@
 		</div>
 
 		<!-- Controls -->
-		<a class="left carousel-control" href="#carousel-example-generic"
-			role="button" data-slide="prev"> <span class="icon-prev"
-			aria-hidden="true"></span> <span class="sr-only">Previous</span>
-		</a> <a class="right carousel-control"
-			href="#carousel-example-generic" role="button" data-slide="next"> <span
-			class="icon-next" aria-hidden="true"></span> <span class="sr-only">Next</span>
+		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev"> <span class="icon-prev" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a> <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next"> <span class="icon-next" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
 		</a>
 	</div>
 </section>
@@ -51,8 +47,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 offset-md-1 col-xs-10 offset-xs-1">
-			<input type="text" id="searchEvent" class="form-control" onkeyup=""
-				placeholder="Search for event..." style="margin: 1.25rem 0.625rem" />
+			<input type="text" id="searchEvent" class="form-control" onkeyup="" placeholder="Search for event..." style="margin: 1.25rem 0.625rem" />
 		</div>
 	</div>
 </div>
@@ -99,10 +94,9 @@
 			}
 			?>
 			</div>
-			<img id="phimg" data-src="holder.js/100px280/thumb" alt="100%x280"
-						style="height: 280px; width: 100%; display: none;"
-						src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22356%22%20height%3D%22280%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20356%20280%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158b0639c79%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A18pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158b0639c79%22%3E%3Crect%20width%3D%22356%22%20height%3D%22280%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22131.2890625%22%20y%3D%22148.1%22%3E356x280%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-						data-holder-rendered="true">			
+			<img id="phimg" data-src="holder.js/100px280/thumb" alt="100%x280" style="height: 280px; width: 100%; display: none;"
+				src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22356%22%20height%3D%22280%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20356%20280%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158b0639c79%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A18pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158b0639c79%22%3E%3Crect%20width%3D%22356%22%20height%3D%22280%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22131.2890625%22%20y%3D%22148.1%22%3E356x280%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+				data-holder-rendered="true">
 		</div>
 	</div>
 </div>
@@ -136,14 +130,14 @@ function searchEvent(ev){
     		  dataType: 'html',
     		  success:function(data){
     			  try{  
+    				  //console.log(data);
     				   var response = jQuery.parseJSON(data);
 
     				   var events = response.events;
 
     				   for (var i in events){
-                            //console.log(events[i].products);
                            if(events[i].products.length > 0){
-                                ceateEvent(events[i].even_name, events[i].even_url, createPic(events[i].products[0].prod_filepath ,events[i].products[0].prod_name));
+                                ceateEvent(events[i].even_name, events[i].even_url, createPic(events[i].products[0].prod_filepath ,events[i].products[0].prod_name, events[0].even_status));
                             } else {
                                 ceateEvent(events[i].even_name, events[i].even_url)
                             }
@@ -161,21 +155,38 @@ function searchEvent(ev){
     		});
     }
     
-function createPic(prodfile, prodname){
-    var img = document.createElement("IMG");
+function createPic(prodfile, prodname, status){
     
-    // Attribute dem bild geben
-    img.classList.add("img-responsive");
-    img.setAttribute("data-src", "./"+prodfile);
-    img.setAttribute("src", "./"+prodfile);
-    img.setAttribute("alt", prodname);
-    img.style.height = "280px";
-    img.style.width = "100%";
-    img.style.filter = "blur(5px)";
-    img.onmouseover = function(){show(this);};
-    img.onmouseout = function(){notShow(this);};
-    
-    return img;
+    if(status == 2){
+    	var i = document.createElement("I");
+        var div = document.createElement("DIV");
+		i.classList.add("fa");
+        i.classList.add("fa-lock");
+        //i.classList.add("fa-10x");
+        i.style.fontSize = "17rem";
+        i.style.display = "inline-block";
+        i.style.width = "100%";
+        i.style.textAlign = "center";
+        i.setAttribute("aria-hidden","true");
+        
+        div.appendChild(i);
+        
+        return div;
+    } else {
+        var img = document.createElement("IMG");
+        
+        img.setAttribute("data-src", "./"+prodfile);
+        img.setAttribute("src", "./"+prodfile);
+        img.style.filter = "blur(5px)";
+        img.classList.add("img-responsive");
+        img.setAttribute("alt", prodname);
+        img.style.height = "280px";
+        img.style.width = "100%";
+        img.onmouseover = function(){show(this);};
+        img.onmouseout = function(){notShow(this);};
+        
+        return img;
+    }
 }
 
 function ceateEvent(eventname, eventurl, img){
@@ -278,10 +289,8 @@ function notShow(img){
     window.addEventListener('load', setPics, false);
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width="356" height="280"
-	viewBox="0 0 356 280" preserveAspectRatio="none"
+<svg xmlns="http://www.w3.org/2000/svg" width="356" height="280" viewBox="0 0 356 280" preserveAspectRatio="none"
 	style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;">
 	<defs>
 	<style type="text/css"></style></defs>
-	<text x="0" y="18"
-		style="font-weight:bold;font-size:18pt;font-family:Arial, Helvetica, Open Sans, sans-serif">356x280</text></svg>
+	<text x="0" y="18" style="font-weight:bold;font-size:18pt;font-family:Arial, Helvetica, Open Sans, sans-serif">356x280</text></svg>
