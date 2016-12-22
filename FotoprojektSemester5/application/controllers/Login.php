6504a784332e5e2c7d51e820198d928d7d0da8fb
alex <alex-finkbeiner@gmx.de>
2016-12-22 09:06:54 +0100

    sperrmeldung wird auf der richtigen view angezeigt

--- FotoprojektSemester5/application/controllers/Login.php
+++ FotoprojektSemester5/application/controllers/Login.php
@@ -125,7 +125,7 @@ public    function confirmAccount($user_confirmcode){
     		//ceck first if user wasn't lockedByAdmin 
     		$uresult = $this->user_model->get_user ( $user_email );
     		if ($uresult[0]->user_status == UserStatus::lockedByAdmin) {
-    			$this->session->set_flashdata('msg', 'Solange ihr Account gesperrt ist können Sie ihr Passwort nicht zurück setzen. Kontaktieren Sie bitte den Admin für weitere Informationen');    			 
+    			$this->session->set_flashdata('pwlocked', 'Solange ihr Account gesperrt ist können Sie ihr Passwort nicht zurück setzen. Kontaktieren Sie bitte den Admin für weitere Informationen');    			 
     			redirect ( "login/forgotPassword" );
     		}
     		else{
