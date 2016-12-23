4d86693c4b89ec2e17ff0cd1b505db0e2a01f4d6
Kai Kleefisch <Kai.Kleefisch@gmx.de>
2016-12-23 09:18:05 +0100

    Success-kontact-view für Alex hinzugefügt

---
+++ FotoprojektSemester5/application/views/user/success_kontact_request_view.php
@@ -0,0 +1,7 @@
+<section class="jumbotron text-xs-center">
+	<div class="container">
+		<h1 class="jumbotron-heading">Vielen Dank</h1>
+		<p class="lead text-muted">Ihre Kontaktanfrage wurde erfolgreich an uns übermittelt.
+		Wir werden Ihre Anfrage schnellstmöglich bearbeiten.</p>
+	</div>
+</section>
