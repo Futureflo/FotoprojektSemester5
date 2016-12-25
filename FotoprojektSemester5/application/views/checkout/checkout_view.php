1133c6e5034f57adae65de43a3d2a5bf35e8247f
Kai Bücking <wi14056@lehre.dhbw-stuttgart.de>
2016-12-25 13:29:11 +0100

    Warenkorb Ansicht angepasst

--- FotoprojektSemester5/application/views/checkout/checkout_view.php
+++ FotoprojektSemester5/application/views/checkout/checkout_view.php
@@ -6,9 +6,7 @@
 			Bilder bestimmen, oder diese aus dem Warenkorb entfernen.</p>
 	</div>
 </section>
-<div class="container">
-	<div class="row">
-		
+<div class="container">	
 		<?php
 		$shoppingcart_positions = $cart->shoppingcart_positions;
 		foreach ( $shoppingcart_positions as $shoppingcart_position ) {
@@ -18,43 +16,50 @@
 			$prodname = $shoppingcart_position->product_variant->prod_name;
 			$size = $shoppingcart_position->product_variant->prty_description;
 			
+			// Event zu Produkt-Variante ermitteln
+			$even_id = $shoppingcart_position->product_variant->prod_even_id;
+			$event = Event::getSingleEventById ( $even_id );
+			
+			// Für jede Warenkorpostion eine neue HTML-Zeile
+			echo '<div class="row" > ';
+			
+			// Spalte 1: Bild
 			echo '<div class="col-sm-2 col-xs-6">';
 			echo '<img src="http://placehold.it/100x100" alt="..."
 					class="img-responsive" />
 					</div>';
 			
+			// Spalte 2: Produktinfo
 			echo '<div class="col-sm-4 col-xs-6">';
-			
 			echo '<ul style="list-style-type: none">';
 			echo '<li><h6>' . $prodname . '</h6></li>';
-			echo '<li>Veranstaltung</li>';
+			echo '<li>Veranstaltung: ' . $event->even_name . '</li>';
 			echo '<li>Größe: ' . $size . '</li>';
 			echo '<li>Digital/Analog</li>';
-			echo '<li><h6>';
+			echo '</div>';
 			
+			// Spalte 3: Preis
 			echo '<div class="col-sm-1"><h5><i id="aktuellerpreis">' . $price . '</i>€</h5></div>';
+			
+			// Spalte 4: Menge
+			echo '<div class="col-sm-1"><h5>Anzahl:</h5> </div>';
+			echo '<div class="form-group col-sm-2">';
+			echo "<p>" . '<input type="text" class="form-control" id="anzahl" value=' . $amount . ' onchange=preisaktualisieren()>' . "</p>";
+			echo "</div>";
+			
+			// Spalte 5: Button
+			echo '<div class="col-sm-1">';
+			echo '<button class="btn btn-danger btn-sm">';
+			echo '<i class="fa fa-trash-o"></i>';
+			echo '</button>';
+			echo '</div>';
+			
+			// Ende der HTML-Zeile
+			echo '</div>';
 		}
 		?>
-		</div>
-		<div class="col-sm-1">
-			<h5>Anzahl:</h5>
-		</div>
-		<div class="form-group col-sm-2">
-			<p>
-				<input type="text" class="form-control" id="anzahl"
-					onchange="preisaktualisieren()">
-			</p>
-		</div>
-		
-		<div class="col-sm-1">
-			<button class="btn btn-danger btn-sm">
-				<i class="fa fa-trash-o"></i>
-			</button>
-		</div>
-	</div>
-	<hr />
-		
-		<div class="row">
+		<hr>
+	<div class="row">
 		<div class="col-sm-0 col-s-0"></div>
 		<div class="col-sm-2 col-xs-10">
 			<h6>Nettopreis:</h6>
@@ -62,7 +67,7 @@
 			<h6>Versandkosten:</h6>
 			<h5>Gesamtpreis:</h5>
 		</div>
-		
+
 		<div class="col-sm-1 col-xs-2">
 			<h6>
 				<i id="nettopreis">1.62</i>€
@@ -74,50 +79,45 @@
 			<h5>
 				<i id="gesamtpreis">2.99</i>€
 			</h5>
-		
-		<a href="#" class="btn btn-success btn-block">Zur Kasse <i class="fa fa-angle-right"></i></a>
 		</div>
 	</div>
+	<div class="row">
+		<a href="#" class="btn btn-success btn-block">Zur Kasse <i
+			class="fa fa-angle-right"></i></a>
+	</div>
+
+
 	<hr />
 
 </div>
 <script>
 function preisaktualisieren()
 {
-var einzelpreis = document.getElementById("einzelpreis").innerHTML;
-var anzahl = document.getElementById("anzahl").value;
-
-if (anzahl < 1) {
+var einzelpreis = document . getElementById ( "einzelpreis" ) . innerHTML;
+var anzahl = document . getElementById ( "anzahl" ) . value;
+			
+			if (anzahl < 1) {
     document.getElementById("anzahl").value = 1;
-} else if (isNaN(anzahl)) {
+			} else if (isNaN ( anzahl )) {
 	document.getElementById("anzahl").value = 1;
-} else {
-	document.getElementById("aktuellerpreis").innerHTML = parseFloat(einzelpreis)*parseFloat(anzahl);
-} 
-nettopreis();
-mehrwertsteuer();
-gesamtpreis();
-}
-
-
-function nettopreis()
-{
-var aktuellerpreis = document.getElementById("aktuellerpreis").innerHTML;
-document.getElementById("nettopreis").innerHTML = parseFloat(aktuellerpreis*0.81).toFixed(2);
-}
-
-function mehrwertsteuer()
-{
-var aktuellerpreis = document.getElementById("aktuellerpreis").innerHTML;
-document.getElementById("mehrwertsteuer").innerHTML = parseFloat(aktuellerpreis*0.19).toFixed(2);
-}
-
-
-function gesamtpreis()
-{
-var aktuellerpreis = document.getElementById("aktuellerpreis").innerHTML;
-document.getElementById("gesamtpreis").innerHTML = parseFloat(aktuellerpreis);
-}
-
-
+			} else {
+	document.getElementById("aktuellerpreis").innerHTML = parseFloat ( einzelpreis ) * parseFloat ( anzahl );
+			}
+			nettopreis ();
+			mehrwertsteuer ();
+			gesamtpreis ();
+		}
+		function nettopreis() {
+var aktuellerpreis = document . getElementById ( "aktuellerpreis" ) . innerHTML;
+document.getElementById("nettopreis").innerHTML = parseFloat ( aktuellerpreis * 0.81 ) . toFixed ( 2 );
+		}
+		function mehrwertsteuer() {
+var aktuellerpreis = document . getElementById ( "aktuellerpreis" ) . innerHTML;
+document.getElementById("mehrwertsteuer").innerHTML = parseFloat ( aktuellerpreis * 0.19 ) . toFixed ( 2 );
+		}
+		function gesamtpreis() {
+var aktuellerpreis = document . getElementById ( "aktuellerpreis" ) . innerHTML;
+document.getElementById("gesamtpreis").innerHTML = parseFloat ( aktuellerpreis );
+		}
 </script>
+
