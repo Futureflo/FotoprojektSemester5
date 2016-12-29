afbc28b59368fc5517c0575eebd0b8d23707e330
Kai Bücking <wi14056@lehre.dhbw-stuttgart.de>
2016-12-29 09:41:46 +0100

    Ändern von Preisprofilen / Zuordnung Format->Preisprofil

--- FotoprojektSemester5/application/controllers/PriceProfile.php
+++ FotoprojektSemester5/application/controllers/PriceProfile.php
@@ -6,7 +6,19 @@ class PriceProfile extends CI_Controller {
 		$this->price_profiles ();
 	}
 	public function showSinglePriceProfile($prpr_id) {
-		$data ['price_profile'] = PriceProfile::getPriceProfile ( $prpr_id );
+		$price_profile = PriceProfile::getPriceProfile ( $prpr_id );
+		
+		$user_id = $price_profile->prpr_user_id;
+		
+		// nicht verwendete IDs speichern und in der Auswahl ausschließen
+		$prty_ids = array ();
+		foreach ( $price_profile->prices as $price ) {
+			array_push ( $prty_ids, $price->prpt_prty_id );
+		}
+		$this->load->model ( 'product_type_model' );
+		$data ['unused_prty'] = $this->product_type_model->getAllUnusedProductTypeByPriceProfile ( $user_id, $prty_ids );
+		
+		$data ['price_profile'] = $price_profile;
 		$this->load->template ( 'price/single_price_profile_view.php', $data );
 	}
 	public function price_profiles() {
@@ -18,17 +30,25 @@ class PriceProfile extends CI_Controller {
 	public static function getAllPriceProfiles() {
 		$CI = & get_instance ();
 		$CI->load->model ( 'PriceProfile_model' );
+		$CI->load->model ( 'User_model' );
 		$user_id = $CI->session->userdata ( 'user_id' );
-		$price_profiles_sys = $CI->PriceProfile_model->getPriceProfilesByUser ( 0 );
-		// Wenn der Benutzer gesetzt ist dann können die Profile geladen werden
-		if ($user_id != 0) {
-			$price_profiles_user = $CI->PriceProfile_model->getPriceProfilesByUser ( $user_id );
-			$price_profiles = array_merge ( $price_profiles_sys, $price_profiles_user );
-		} else
-			$price_profiles = $price_profiles_sys;
 		
-		foreach ( $price_profiles as $price_profile ) {
-			$price_profile = PriceProfile::getPriceProfile ( $price_profile->prpr_id );
+		$user = $CI->User_model->get_user_by_id ( $user_id );
+		if ($user [0]->user_role_id == UserRole::Admin)
+			$price_profiles = $CI->PriceProfile_model->getAllPriceProfiles ();
+		else {
+			$price_profiles_sys = $CI->PriceProfile_model->getPriceProfilesByUser ( 0 );
+			
+			// Wenn der Benutzer gesetzt ist dann können die Profile geladen werden
+			if ($user_id != 0) {
+				$price_profiles_user = $CI->PriceProfile_model->getPriceProfilesByUser ( $user_id );
+				$price_profiles = array_merge ( $price_profiles_sys, $price_profiles_user );
+			} else
+				$price_profiles = $price_profiles_sys;
+			
+			foreach ( $price_profiles as $price_profile ) {
+				$price_profile = PriceProfile::getPriceProfile ( $price_profile->prpr_id );
+			}
 		}
 		
 		return $price_profiles;
@@ -54,5 +74,17 @@ class PriceProfile extends CI_Controller {
 		} else {
 			return NULL;
 		}
+	}
+	public function addPriceProductType() {
+		$this->load->model ( 'PriceProfile_model' );
+		$prpr_id = $this->input->post ( 'prpt_prpr_id' );
+		$data = array (
+				'prpt_prpr_id' => $prpr_id,
+				'prpt_prty_id' => $this->input->post ( 'prpt_prty_id' ),
+				'prpt_price' => $this->input->post ( 'prpt_price' ) 
+		);
+		
+		$price_profile = $this->PriceProfile_model->insert_price_product_type ( $data );
+		redirect ( 'PriceProfile/showSinglePriceProfile/' . $prpr_id );
 	}
 }
--- FotoprojektSemester5/application/models/PriceProfile_model.php
+++ FotoprojektSemester5/application/models/PriceProfile_model.php
@@ -1,55 +1,56 @@
-<?php 
-   Class PriceProfile_model extends CI_Model {
+<?php
+class PriceProfile_model extends CI_Model {
+	Public function __construct() {
+		parent::__construct ();
+	}
+	Public function getAllPriceProfiles() {
+		$query = $this->db->get ( "price_profile" );
+		return $query->result ();
+	}
+	Public function getPriceProfilesByUser($user_id) {
+		$this->db->where ( 'prpr_user_id', $user_id );
+		$query = $this->db->get ( "price_profile" );
+		return $query->result ();
+	}
+	Public function getPriceProfileById($prpr_id) {
+		$this->db->where ( 'prpr_id', $prpr_id );
+		$query = $this->db->get ( "price_profile" );
+		return $query->result ();
+	}
+	Public function getPricesById($prpr_id) {
+		$this->db->join ( 'product_type', 'prty_id = prpt_prty_id', 'INNER' );
+		$this->db->where ( 'prpt_prpr_id', $prpr_id );
+		$query = $this->db->get ( "price_product_type" );
+		return $query->result ();
+	}
+	Public function getPriceByProductType($prpr_id, $prty_id) {
+		$this->db->where ( 'prpt_prpr_id', $prpr_id );
+		$this->db->where ( 'prpt_prty_id', $prty_id );
+		$query = $this->db->get ( "price_product_type" );
+		return $query->result ();
+	}
 	
-      Public function __construct() { 
-         parent::__construct(); 
-      } 
-      
-      Public function getPriceProfilesByUser($user_id){
-      	$this->db->where('prpr_user_id', $user_id);
-      	$query = $this->db->get("price_profile");
-      	return $query->result();
-      }
-      
-      Public function getPriceProfileById($prpr_id){
-      	$this->db->where('prpr_id', $prpr_id);
-      	$query = $this->db->get("price_profile");
-      	return $query->result();
-      }
-      
-      Public function getPricesById($prpr_id){
-      	$this->db->join('product_type', 'prty_id = prpt_prty_id', 'INNER');
-      	$this->db->where('prpt_prpr_id', $prpr_id);
-      	$query = $this->db->get("price_product_type");
-      	return $query->result();
-      }
-      
-      Public function getPriceByProductType($prpr_id, $prty_id){
-      	$this->db->where('prpt_prpr_id', $prpr_id);
-      	$this->db->where('prpt_prty_id', $prty_id);
-      	$query = $this->db->get("price_product_type");
-      	return $query->result();
-      }
-      
-
-      // insert price_profile
-      function insert_price_profile($data)
-      {
-      	return $this->db->insert('product', $data);
-      }
-      
-      //delete price_profile
-      function update_price_profile($prpr_id, $data)
-      {
-      	$this->db->where('prpr_id', $prpr_id);
-      	$this->db->update('price_profile', $data);
-      }
-      
-      //delete price_profile
-      function delete_price_profile($prpr_id)
-      {
-      	return $this->db->delete('price_profile', array('prpr_id' => $prpr_id));
-      }
-     		
-   } 
+	// insert price_profile
+	function insert_price_profile($data) {
+		return $this->db->insert ( 'product', $data );
+	}
+	
+	// delete price_profile
+	function update_price_profile($prpr_id, $data) {
+		$this->db->where ( 'prpr_id', $prpr_id );
+		$this->db->update ( 'price_profile', $data );
+	}
+	
+	// delete price_profile
+	function delete_price_profile($prpr_id) {
+		return $this->db->delete ( 'price_profile', array (
+				'prpr_id' => $prpr_id 
+		) );
+	}
+	
+	// insert price_product_type
+	function insert_price_product_type($data) {
+		return $this->db->insert ( 'price_product_type', $data );
+	}
+}
 ?>
