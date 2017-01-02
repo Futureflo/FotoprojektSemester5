786541f998790e49584befdd06e1ee46b542416f
Tim Deisser <tim.deisser@gmx.de>
2017-01-02 20:41:44 +0100

    Page Not Found angepasst

--- FotoprojektSemester5/application/views/errors/html/error_404.php
+++ FotoprojektSemester5/application/views/errors/html/error_404.php
@@ -1,64 +1,52 @@
 
 
 <style type="text/css">
-
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
+::selection {
+	background-color: #E13300;
+	color: white;
 }
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
+::-moz-selection {
+	background-color: #E13300;
+	color: white;
 }
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
+.error-template {
+	padding: 40px 15px;
+	text-align: center;
 }
 
-#text {
-	color: #444;
-	background-color: transparent;
-	font-size: 16px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
+.error-actions {
+	margin-top: 15px;
+	margin-bottom: 15px;
 }
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
+.error-actions .btn {
+	margin-right: 10px;
 }
 </style>
-	<div id="container" style="margin: 4rem 0rem 0rem 0rem;">
-		<h1>404 Page Not Found</h1>
-		<i id="text">The page you requested was not found.</i>
+
+<section style="margin: 5rem 0rem 0rem 0rem;"></section>
+
+
+<div class="container">
+	<div class="row">
+		<div class="error-template">
+			<h1>Oops!</h1>
+			<h2>404 Page Not Found</h2>
+			<div class="error-details">
+				Es tut uns leid, ein Fehler ist passiert, die angeforderte Seite kann nicht gefunden werden!<br>
+			</div>
+			<div class="error-actions">
+				<a href="<?php
+				echo base_url ();
+				?>" class="btn btn-primary"> <i class="fa fa-home" aria-hidden="true"></i> Auf die Startseite
+				</a> <a href="<?php
+				
+				echo base_url ();
+				?>contact" class="btn btn-secondary"> <i class="fa fa-envelope" aria-hidden="true"></i> Kontaktieren Sie uns
+				</a>
+			</div>
+		</div>
 	</div>
+</div>
