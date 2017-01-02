1df0a9e829ac6fc1c27fe1ea8be74dbba261f2e2
Kai Kleefisch <Kai.Kleefisch@gmx.de>
2017-01-02 20:43:25 +0100

    Succes view Registrierung hinzugefügt

---
+++ FotoprojektSemester5/application/views/user/success_signup_view.php
@@ -0,0 +1,6 @@
+<section class="jumbotron text-xs-center">
+	<div class="container">
+		<h1 class="jumbotron-heading">Vielen Dank</h1>
+		<p class="lead text-muted">Wir haben Ihnen eine E-Mail mit einem Bestätigungslink gesendet. Bitte bestätigen Sie die E-Mail Adresse.</p>
+	</div>
+</section>
