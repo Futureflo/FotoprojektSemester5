6af20d1d382b44fca737f653756247e0c2497478
Tim Deisser <tim.deisser@gmx.de>
2017-01-06 16:41:54 +0100

    hidden field for Gewerbeschein upload

--- FotoprojektSemester5/application/views/user/signup_view.php
+++ FotoprojektSemester5/application/views/user/signup_view.php
@@ -1,166 +1,172 @@
 <section class="jumbotron text-xs-left">
 	<div class="container">
 
-								<div id="fehler_span" class="text-danger"><?php
-								
-								echo $this->session->flashdata ( 'msgReg' );
-								?></div>
-	
+		<div id="fehler_span" class="text-danger"><?php
+		echo $this->session->flashdata ( 'msgReg' );
+		?></div>
+
 		<div class="row">
 			<ul class="nav nav-pills" role="tablist">
-				<li class="nav-item"><a class="nav-link active" href="#customer"
-					data-toggle="tab" onmouseout="checkType()">Kunde</a></li>
-				<li class="nav-item"><a class="nav-link" href="#photographer"
-					data-toggle="tab" onmouseout="checkType()">Fotograf</a></li>
+				<li class="nav-item"><a class="nav-link active" href="#customer" data-toggle="tab" onmouseout="checkType()">Kunde</a></li>
+				<li class="nav-item"><a class="nav-link" href="#photographer" data-toggle="tab" onmouseout="checkType()">Fotograf</a></li>
 			</ul>
 					<?php
- 					echo form_open ('signup/');
+					echo form_open ( 'signup/' );
 					?>
 
 				<div class="form-group">
-					<fieldset class="form-group">
+				<fieldset class="form-group">
+					<div class="row">
+						<div class="col-sm-12">
+							<h3 class="jumbotron-heading">Persönliche Angaben:</h3>
+							<br>
+						</div>
+					</div>
+					<div class="form-check">
 						<div class="row">
-							<div class="col-sm-12">
-								<h3 class="jumbotron-heading">Persönliche Angaben:</h3>
-								<br>
+							<div class="col-sm-3">
+								<label class="form-check-label"> <input type="radio" class="form-check-input" name="gender" value="Frau" checked> Frau
+								</label> <label class="form-check-label"> <input type="radio" class="form-check-input" name="gender" value="Herr"> Herr
+								</label>
 							</div>
 						</div>
-						<div class="form-check">
-							<div class="row">
-								<div class="col-sm-3">
-									<label class="form-check-label"> <input type="radio"
-										class="form-check-input" 
-										name="gender" value="Frau" checked> Frau
-									</label> 
-									<label class="form-check-label"> <input type="radio"
-										class="form-check-input"
-										name="gender" value="Herr"> Herr
-									</label>
-								</div>
-							</div>
-						</div>
-					</fieldset>
+					</div>
+				</fieldset>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="text" class="form-control" name="firstname" placeholder="Vorname"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'firstname' );
+							?>
+							</span>
 				</div>
-				<div class="row">
-					<div class="col-sm-12">
-						<input type="text" class="form-control" name="firstname"
-							placeholder="Vorname">
-							<span class="text-danger">
-							<?php echo form_error ( 'firstname' );?>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="text" class="form-control" name="lastname" placeholder="Nachname"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'lastname' );
+							?>
+							</span>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="form-group">
+						<select class="form-control" name="country">
+							<option>Deutschland</option>
+						</select> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'country' );
+							?>
 							</span>
 					</div>
 				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-12">
-						<input type="text" class="form-control" name="lastname"
-							placeholder="Nachname">
-							<span class="text-danger">
-							<?php echo form_error ( 'lastname' );?>
-							</span>							
-					</div>
-				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="form-group">
-							<select class="form-control" name="country">
-								<option>Deutschland</option>
-							</select>
-									<span class="text-danger">
-							<?php echo form_error ( 'country' );?>
-							</span>	
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-3 col-xs-3">
-						<input type="text" class="form-control" name="zip"
-							placeholder="PLZ">
-							<span class="text-danger">
-							<?php echo form_error ( 'zip' );?>
-							</span>							
-					</div>
-					<div class="col-sm-9 col-xs-9">
-						<input type="text" class="form-control" name="city"
-							placeholder="Ort">
-							<span class="text-danger">
-							<?php echo form_error ( 'city' );?>
-							</span>							
-					</div>
-				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-9 col-xs-9">
-						<input type="text" class="form-control" name="street"
-							placeholder="Straße">
-							<span class="text-danger">
-							<?php echo form_error ( 'street' );?>
+			</div>
+			<div class="row">
+				<div class="col-sm-3 col-xs-3">
+					<input type="text" class="form-control" name="zip" placeholder="PLZ"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'zip' );
+							?>
 							</span>
-					</div>
-					<div class="col-sm-3 col-xs-3">
-						<input type="text" class="form-control" name="housenumber"
-							placeholder="Hausnr">
-							<span class="text-danger">
-							<?php echo form_error ( 'housenumber' );?>
-							</span>
-					</div>
 				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-12">
-						<!--  <input type="date" class="form-control input-sm chat-input" placeholder="Datum" name="even_date" value="
+				<div class="col-sm-9 col-xs-9">
+					<input type="text" class="form-control" name="city" placeholder="Ort"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'city' );
+							?>
+							</span>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-9 col-xs-9">
+					<input type="text" class="form-control" name="street" placeholder="Straße"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'street' );
+							?>
+							</span>
+				</div>
+				<div class="col-sm-3 col-xs-3">
+					<input type="text" class="form-control" name="housenumber" placeholder="Hausnr"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'housenumber' );
+							?>
+							</span>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<!--  <input type="date" class="form-control input-sm chat-input" placeholder="Datum" name="even_date" value="
 						<?php
 						echo set_value ( 'birthday' );
 						?>"/>  -->
-						<input class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" 
-							placeholder="Geburtstag" name="birthday" />
-							 <span class="text-danger">
-							<?php echo form_error ( 'birthday' );?>
+					<input class="form-control" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" placeholder="Geburtstag" name="birthday" /> <span
+						class="text-danger">
+							<?php
+							
+							echo form_error ( 'birthday' );
+							?>
 							</span>
-					</div>
 				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-12">
-						<input type="email" class="form-control" name="email"
-							placeholder="E-Mail">
-							<span class="text-danger">
-							<?php echo form_error ( 'email' );?>
-							</span>							
-					</div>
-				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-12">
-						<input type="email" class="form-control" name="cemail"
-							placeholder="E-Mail wiederholen">
-							<span class="text-danger">
-							<?php echo form_error ( 'cemail' );?>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="email" class="form-control" name="email" placeholder="E-Mail"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'email' );
+							?>
 							</span>
-					</div>
 				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-12">
-						<input class="form-control" type="password" name="password"
-							placeholder="Passwort">
-							<span class="text-danger">
-							<?php echo form_error ( 'password' );?>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="email" class="form-control" name="cemail" placeholder="E-Mail wiederholen"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'cemail' );
+							?>
 							</span>
-					</div>
 				</div>
-				<br>
-				<div class="row">
-					<div class="col-sm-12">
-						<input class="form-control" type="password" name="cpassword"
-							placeholder="Passwort wiederholen">
-							<span class="text-danger">
-							<?php echo form_error ( 'cpassword' );?>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input class="form-control" type="password" name="password" placeholder="Passwort"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'password' );
+							?>
 							</span>
-					</div>
 				</div>
-			
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input class="form-control" type="password" name="cpassword" placeholder="Passwort wiederholen"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'cpassword' );
+							?>
+							</span>
+				</div>
+			</div>
+
 
 			<!-- Tab panes -->
 			<div class="tab-content">
@@ -168,108 +174,116 @@
 					<!-- Der Inhalt ist leer, da die Kunden Felder immer angezeigt werden -->
 				</div>
 				<div role="tabpanel" class="tab-pane" id="photographer">
-					
-						<br>
-						<div id="signupphotographer">
-							<div class="row">
-								<div class="col-sm-12">
-									<div class="form-insert_product">
+
+					<br>
+					<div id="signupphotographer">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="form-insert_product">
 									<?php
 									
 									echo form_open_multipart ( 'signup/uploadTradeLicense' );
 									?>
-										<input type="file" name="dateiupload" /> <input
-											type="submit" name="btn[upload]" class="btn btn-success"
-											value="Gewerbeschein absenden" />
-							<span class="text-danger">
-							<?php echo form_error ( 'dateiupload' );?>
-							</span>
-									</div>
-								</div>
-							</div>
-							<br>
-							<div class="row">
-								<div class="col-sm-12">
-									<input type="text" class="form-control" name="accountholder"
-										placeholder="Kontoinhaber">
-							<span class="text-danger">
-							<?php echo form_error ( 'accountholder' );?>
-							</span>
-								</div>
-							</div>
-							<br>
-							<div class="row">
-								<div class="col-sm-12">
-									<input type="text" class="form-control" name="iban"
-										placeholder="IBAN">
-							<span class="text-danger">
-							<?php echo form_error ( 'iban' );?>
-							</span>										
-								</div>
-							</div>
-							<br>
-							<div class="row">
-								<div class="col-sm-12">
-									<input type="text" class="form-control" name="bic"
-										placeholder="BIC">
-							<span class="text-danger">
-							<?php echo form_error ( 'bic' );?>
+										<div class="col-sm-6"><input type="file" name="dateiupload" onchange="checkFile(this)"/></div> <div class="col-sm-2"><input type="hidden" name="uploadcheck" id="uploadcheck" value="notChecked"/><i id="check" class="fa fa-check" aria-hidden="true" style="display: none"></i></div> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'dateiupload' );
+							?>
 							</span>
 								</div>
 							</div>
 						</div>
-					
+						<br>
+						<div class="row">
+							<div class="col-sm-12">
+								<input type="text" class="form-control" name="accountholder" placeholder="Kontoinhaber"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'accountholder' );
+							?>
+							</span>
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-sm-12">
+								<input type="text" class="form-control" name="iban" placeholder="IBAN"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'iban' );
+							?>
+							</span>
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-sm-12">
+								<input type="text" class="form-control" name="bic" placeholder="BIC"> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'bic' );
+							?>
+							</span>
+							</div>
+						</div>
+					</div>
+
 				</div>
 				<br>
-				
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="form-check">
-								<label class="form-check-label"> <input type="checkbox"
-									class="form-check-input" name="checktermsandconditions"> AGB zustimmen
-								</label>
-							<span class="text-danger">
-							<?php echo form_error ( 'checktermsandconditions' );?>
+
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="form-check">
+							<label class="form-check-label"> <input type="checkbox" class="form-check-input" name="checktermsandconditions"> AGB zustimmen
+							</label> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'checktermsandconditions' );
+							?>
 							</span>
-							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="form-check">
-								<label class="form-check-label"> <input type="checkbox"
-									class="form-check-input" name="checklegalnotice"> Datenschutzrichtlinien akzeptieren
-								</label>
-							<span class="text-danger">
-							<?php echo form_error ( 'checklegalnotice' );?>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="form-check">
+							<label class="form-check-label"> <input type="checkbox" class="form-check-input" name="checklegalnotice"> Datenschutzrichtlinien akzeptieren
+							</label> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'checklegalnotice' );
+							?>
 							</span>
-							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="form-check">
-								<label class="form-check-label"> <input type="checkbox"
-									class="form-check-input" name="checknewsletter"> Newsletter abonnieren
-								</label>
-							<span class="text-danger">
-							<?php echo form_error ( 'checknewsletter' );?>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="form-check">
+							<label class="form-check-label"> <input type="checkbox" class="form-check-input" name="checknewsletter"> Newsletter abonnieren
+							</label> <span class="text-danger">
+							<?php
+							
+							echo form_error ( 'checknewsletter' );
+							?>
 							</span>
-							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-sm-4"></div>
-						<div class="col-sm-6">
-							<button name="submit" type="submit"
-								class="btn btn-primary btn-md">Registrieren</button>
-						</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4"></div>
+					<div class="col-sm-6">
+						<button name="submit" type="submit" class="btn btn-primary btn-md">Registrieren</button>
 					</div>
-				
+				</div>
+
 
 			</div>
 			<input id="type_hidden_field" type="hidden" name="type_hidden_field" value="">
-			<?php echo form_close();?>
+			<?php
+			
+			echo form_close ();
+			?>
 		</div>
 	</div>
 </section>
@@ -278,7 +292,18 @@
 
 
 <script type="text/javascript">
-    
+  function checkFile(i){
+	var check = document.getElementById('check');
+	var hidden = document.getElementById('uploadcheck');
+
+	if(i.value.length > 0){
+		check.style.display = "block";
+		hidden.value = "checked";
+	} else {
+		check.style.display = "none";
+		hidden.value = "notChecked";
+	}
+  }  
 
 	function checkType(){
         var customer = document.getElementById('customer');
