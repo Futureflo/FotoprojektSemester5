8d566d9b29bdd9f26959c05dc746138b7d2474aa
Futureflo <wi14138@lehre.dhbw-stuttgart.de>
2017-01-09 09:54:55 +0100

    View zum Abmelden vom Newsletter hinzugefügt

---
+++ FotoprojektSemester5/application/views/newsletter/newsletterunregister_view.php
@@ -0,0 +1,26 @@
+<section class="jumbotron text-xs-center">
+	<section class="text-xs-left">
+		<div class="container">
+			<?php
+			echo form_open ( 'Newsletter/' );
+			?>
+			<div class="">
+				<h2>Newsletterabmeldung:</h2>
+				<input type="text" class="form-control input-sm chat-input" placeholder="E-Mail Adresse*" name="email"/>
+				<br />
+				<div class="form-group">
+			   
+			  </div>
+				
+				<div id="fehler_span" class="text-danger"><?php
+				
+				echo $this->session->flashdata ( 'newsletterMsg' );
+				?></div>
+				<span class="group-btn">    
+		            	<button name="submit" type="submit" class="btn btn-primary btn-md">Abmelden</button>
+		        </span>
+			</div>
+		</div>
+
+	</section>
+</section>
