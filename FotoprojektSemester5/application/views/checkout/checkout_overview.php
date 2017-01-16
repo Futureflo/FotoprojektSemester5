00692b41e6c2a31b58ef67ad7d8295fd8afa117a
Tim Deisser <tim.deisser@gmx.de>
2017-01-16 10:02:22 +0100

    CHeckout Layout angepasst

--- FotoprojektSemester5/application/views/checkout/checkout_overview.php
+++ FotoprojektSemester5/application/views/checkout/checkout_overview.php
@@ -15,7 +15,80 @@ echo form_open ( "Order/newOrder", '', array (
 <div class="container">
 	<div class="row">
 
-		<div class="col-md-10 offset-md-1 col-lg-10 offset-lg-1">
+		<div class="col-md-6 col-xs-12">
+
+			<!-- Postionen -->
+			<!--  <h5>Bestellung überprüfen</h5>-->
+
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Bild</th>
+						<th>Produktinfo</th>
+						<th>Anzahl</th>
+						<th>Preis</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+		<?php
+		$shoppingcart_positions = $cart->shoppingcart_positions;
+		foreach ( $shoppingcart_positions as $shoppingcart_position ) {
+			
+			$price = $shoppingcart_position->product_variant->price ['price_sum'];
+			$amount = $shoppingcart_position->scpo_amount;
+			$prodname = $shoppingcart_position->product_variant->prod_name;
+			$size = $shoppingcart_position->product_variant->prty_description;
+			$prty_type = $shoppingcart_position->product_variant->prty_type;
+			
+			// Event zu Produkt-Variante ermitteln
+			$even_id = $shoppingcart_position->product_variant->prod_even_id;
+			$event = Event::getSingleEventById ( $even_id );
+			
+			// Für jede Warenkorpostion eine neue HTML-Zeile
+			echo "<tr>";
+			
+			// Spalte 1: Bild
+			echo "<td>";
+			echo '<img src="http://placehold.it/100x100" alt="..." class="img-responsive" />';
+			echo "</td>";
+			
+			// Spalte 2: Produktinfo
+			echo '<td>' . $event->even_name . '<br/>';
+			echo '' . $prodname . '<br/>';
+			echo '' . $size . '<br/>';
+			if ($prty_type == ProductPrintType::print) {
+				echo '' . 'Druck' . '<br/>';
+			} else {
+				echo '' . 'Download' . '</td>';
+			}
+			echo '<td>' . $amount . '</td>';
+			echo '<td class="sum">' . $price * $amount . '€</td>';
+			
+			// // Spalte 3: Preis
+			// echo '<div class="col-sm-1"><h5><i id="aktuellerpreis">' . $price . '</i>€</h5></div>';
+			
+			// // Spalte 4: Menge
+			// echo '<div class="col-sm-1"><h5>Anzahl:</h5> </div>';
+			// echo '<div class="form-group col-sm-2">';
+			// echo "<p>" . '<input type="text" class="form-control" id="anzahl" value=' . $amount . ' onchange=preisaktualisieren()>' . "</p>";
+			// echo "</div>";
+			
+			// Spalte 5: Button
+			echo '<td>';
+			echo '<button class="btn btn-danger btn-sm">';
+			echo '<i class="fa fa-trash-o"></i>';
+			echo '</button>';
+			echo '</td>';
+			
+			echo "</tr>";
+		}
+		?>
+					</tbody>
+			</table>
+		</div>
+		
+		<div class="col-md-6 col-xs-12">
 			<div class="form-group col-xs-12">
 				<label class="h5" for="delivery">Lieferadresse</label>
 			    <?php
@@ -53,84 +126,24 @@ echo form_open ( "Order/newOrder", '', array (
 					echo "<option> PayPal </option>";
 					echo "</select>";
 					?>
+					<hr>
 			  </div>
-		</div>
-	</div>
-	<hr>
-	<br>
-	<div class="row">
-		<div class="col-xs-12">
-
-			<!-- Postionen -->
-			<h5>Übersicht</h5>
-
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th></th>
-						<th>Bild</th>
-						<th>Veranstaltung</th>
-						<th>Größe</th>
-						<th>Digital/Analog</th>
-						<th>Preis</th>
-						<th>Menge</th>
-						<th><h5>
-								<b>Summe</b>
-							</h5></th>
-					</tr>
-				</thead>
-				<tbody>
-		<?php
-		$shoppingcart_positions = $cart->shoppingcart_positions;
-		foreach ( $shoppingcart_positions as $shoppingcart_position ) {
-			
-			$price = $shoppingcart_position->product_variant->price ['price_sum'];
-			$amount = $shoppingcart_position->scpo_amount;
-			$prodname = $shoppingcart_position->product_variant->prod_name;
-			$size = $shoppingcart_position->product_variant->prty_description;
-			$prty_type = $shoppingcart_position->product_variant->prty_type;
-			
-			// Event zu Produkt-Variante ermitteln
-			$even_id = $shoppingcart_position->product_variant->prod_even_id;
-			$event = Event::getSingleEventById ( $even_id );
-			
-			// Für jede Warenkorpostion eine neue HTML-Zeile
-			echo "<tr>";
-			
-			// Spalte 1: Bild
-			echo "<td>";
-			// echo '<img src="http://placehold.it/100x100" alt="..."
-			// class="img-responsive" />';
-			echo "</td>";
-			
-			// Spalte 2: Produktinfo
-			echo '<td>' . $prodname . '</td>';
-			echo '<td>' . $event->even_name . '</td>';
-			echo '<td>' . $size . '</td>';
-			if ($prty_type == ProductPrintType::print) {
-				echo '<td>' . 'Druck' . '</td>';
-			} else {
-				echo '<td>' . 'Download' . '</td>';
-			}
-			echo '<td>' . $price . '€</td>';
-			echo '<td>' . $amount . '</td>';
-			echo '<td><h5><b>' . $price * $amount . '€</b></h5></td>';
-			
-			// // Spalte 3: Preis
-			// echo '<div class="col-sm-1"><h5><i id="aktuellerpreis">' . $price . '</i>€</h5></div>';
-			
-			// // Spalte 4: Menge
-			// echo '<div class="col-sm-1"><h5>Anzahl:</h5> </div>';
-			// echo '<div class="form-group col-sm-2">';
-			// echo "<p>" . '<input type="text" class="form-control" id="anzahl" value=' . $amount . ' onchange=preisaktualisieren()>' . "</p>";
-			// echo "</div>";
-			
-			// Ende der HTML-Zeile
-			echo "</tr>";
-		}
-		?>
-					</tbody>
-			</table>
+			  
+			  
+			  <div class="col-xs-12">
+				<div class="col-xs-7">
+					<h6>Nettopreis:</h6>
+					<h6>Mehrwertsteuer(19%):</h6>
+					<h6>Versandkosten:</h6>
+					<h5>Gesamtpreis:</h5>
+				</div>			  
+				<div class="col-xs-5">
+					<h6 align="right" id="netto"></h6>
+					<h6 align="right" id="taxes"></h6>
+					<h6 align="right">0,00 €</h6>
+					<h5 align="right" onload="sum()" id="endSum"></h5>
+				</div>
+			</div>
 		</div>
 
 	</div>
@@ -153,4 +166,29 @@ echo form_open ( "Order/newOrder", '', array (
 <?php
 echo form_close ();
 ?>
+
+<script type="text/javascript">
+function sum(f){
+	var sums = document.getElementsByClassName('sum');
+	var sum = 0;
+	var endSum = document.getElementById('endSum');
+
+	for(var i = 0; i < sums.length; i++){
+		sum += parseFloat(sums[i].innerHTML);
+	}
+	endSum.innerHTML = sum + ' €';
+	otherPrice(sum);
+}
+
+function otherPrice(s){
+	var netto = document.getElementById('netto');
+	var taxes = document.getElementById('taxes');
+
+	netto.innerHTML = parseFloat ( s * 0.81 ) . toFixed ( 2 ) + ' €';
+	taxes.innerHTML = parseFloat ( s * 0.19 ) . toFixed ( 2 ) + ' €';
+}
+
+window.addEventListener('load', sum, false);
+
+</script>
 
