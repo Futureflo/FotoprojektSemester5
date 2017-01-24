41b3db04dfce680ae4f3b0e4ffa39cdd28cef960
Daniel K <daniel.kmiotek@gmx.de>
2017-01-24 13:05:34 +0100

    Fix Dashboard

---
+++ FotoprojektSemester5/application/models/Dashboard_model.php
@@ -0,0 +1,13 @@
+<?php
+class Dashboard_model extends CI_Model {
+	Public function __construct() {
+		parent::__construct ();
+	}
+
+	public function getInformations() {
+		$this->db->join ( 'user', 'orde_user_id = user_id' );
+		$query = $this->db->get ( "order" );
+		return $query->result ();
+	}
+}
+?>
