6ac41ff65bacc015dab53af73665f0b0326a7433
Futureflo <wi14138@lehre.dhbw-stuttgart.de>
2017-01-24 21:34:03 +0100

    Übersicht für Admin hinzugefügt und CSS angepasst

---
+++ FotoprojektSemester5/application/views/admin/overviewAdmin.php
@@ -0,0 +1,98 @@
+<section style="padding-top: 70px"></section>
+<div class="contrainer">
+	<div class="row">
+		<h1 class="offset-md-1 col-md-2">Dashboard</h1>
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="row">
+
+		<a class="btn btn-info kachel "
+			href="<?php
+			echo base_url ();
+			?>admin/users/">
+				<i class="fa fa-user fa-5x" aria-hidden="true"></i> <br><h3>Benutzer</h3> 
+			</a> 
+			
+			
+			<a class="btn btn-warning kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/archivedUsers/">
+				<i class="fa fa-user fa-5x" aria-hidden="true"></i> <br><h3>Benutzer Archiv</h3> 
+			</a>
+			
+			<a class="btn btn-info kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/events_view/">
+				<i class="fa fa-calendar fa-5x" aria-hidden="true"></i> <br><h3>Events</h3> 
+			</a>
+			
+			<a class="btn btn-warning kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/archivedEvents/">
+				<i class="fa fa-calendar fa-5x" aria-hidden="true"></i> <br><h3>Events Archiv</h3> 
+			</a>
+			
+			<a class="btn btn-info kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/printers_view/">
+				<i class="fa fa-print fa-5x" aria-hidden="true"></i> <br><h3>Druckereien</h3> 
+			</a>
+			
+			
+			<a class="btn btn-warning kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/archivedPrinters/">
+				<i class="fa fa-print fa-5x" aria-hidden="true"></i> <br><h3>Druckereien Archiv</h3> 
+			</a>
+			
+			<a class="btn btn-info kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/price_profile_view/">
+				<i class="fa fa-money fa-5x" aria-hidden="true"></i> <br><h3>Preisprofile</h3> 
+			</a>
+			
+			<a class="btn btn-warning kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/archivedPrice_profiles/">
+				<i class="fa fa-money fa-5x" aria-hidden="true"></i> <br><h3>Preisprofile Archiv</h3> 
+			</a>
+			
+			<a class="btn btn-info kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/product_type_view/">
+				<i class="fa fa-file-text-o fa-5x" aria-hidden="true"></i> <br><h3>Formate</h3> 
+			</a>
+			
+			<a class="btn btn-warning kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/archivedProduct_types/">
+				<i class="fa fa-file-text-o fa-5x" aria-hidden="true"></i> <br><h3>Formate Archiv</h3> 
+			</a>
+			
+			<a class="btn btn-info kachel"
+			href="<?php
+			echo base_url ();
+			?>admin/nele_users/">
+				<i class="fa fa-envelope-o fa-5x" aria-hidden="true"></i> <br><h3>Newsletter</h3> 
+			</a>
+		
+	</div>
+</div>
+
+
+
+<?php
+
+?>
