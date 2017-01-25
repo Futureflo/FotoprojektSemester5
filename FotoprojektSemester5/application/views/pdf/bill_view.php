9a3176ad7d30f7bdc1e40f06d704b51b8464a47d
Kai Bücking <wi14056@lehre.dhbw-stuttgart.de>
2017-01-25 20:59:12 +0100

    TestRechnung Merge Severin

--- FotoprojektSemester5/application/views/pdf/bill_view.php
+++ FotoprojektSemester5/application/views/pdf/bill_view.php
@@ -30,6 +30,28 @@
 // require_once('examples/tcpdf_include.php');
 
 
+/**
+ * 	KLEINBETRAGSRECHNUNGEN (kleiner 150€):
+ * 	vollständiger Name und vollständige Anschrift des leistenden Unternehmers,
+ *	das Ausstellungsdatum der Rechnung,
+ *	Menge und Art der gelieferten Gegenstände oder die Art und den Umfang der sonstigen Leistung
+ *	Entgelt und Steuerbetrag für die Lieferung oder Leistung in einer Summe,
+ *	Steuersatz oder
+ *	im Fall einer Steuerbefreiung ein Hinweis darauf, dass für die Lieferung oder sonstige Leistung eine Steuerbefreiung gilt.
+ *	
+ *	NORMALE RECHNUNGEN 
+ *	Vollständiger Name und Anschrift des leistenden Unternehmers und des Leistungsempfängers
+ *	Steuernummer oder Umsatzsteueridentifikationsnummer
+ *	Ausstellungsdatum der Rechnung
+ *	Fortlaufende Rechnungsnummer
+ *	Menge und handelsübliche Bezeichnung der gelieferten Gegenstände oder die Art und den Umfang der sonstigen Leistung
+ *	Zeitpunkt der Lieferung bzw. Leistung
+ *	Nach Steuersätzen und -befreiungen aufgeschlüsseltes Entgelt
+ *	Im Voraus vereinbarte Minderungen des Entgelts
+ *	Entgelt und hierauf entfallender Steuerbetrag sowie Hinweis auf Steuerbefreiung
+ *	Ggf. Hinweis auf Steuerschuld des Leistungsempfängers
+ */
+
 // Extend the TCPDF class to create custom Header and Footer
 class MYPDF extends TCPDF {
 
@@ -63,10 +85,10 @@ $this->tcPDFstart = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, t
 
 // set document information
 $this->tcPDFstart->SetCreator(PDF_CREATOR);
-$this->tcPDFstart->SetAuthor('Nicola Asuni');
-$this->tcPDFstart->SetTitle('TCPDF Example 003');
-$this->tcPDFstart->SetSubject('TCPDF Tutorial');
-$this->tcPDFstart->SetKeywords('TCPDF, PDF, example, test, guide');
+$this->tcPDFstart->SetAuthor('SnapUp');
+$this->tcPDFstart->SetTitle('Bill');
+$this->tcPDFstart->SetSubject('Bill');
+$this->tcPDFstart->SetKeywords('TCPDF, PDF, Bill, Rechnung');
 
 // set default header data
 $this->tcPDFstart->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
@@ -109,14 +131,39 @@ $this->tcPDFstart->AddPage();
 
 // Custom page header and footer are defined by extending the TCPDF class and overriding the Header() and Footer() methods.
 // EOD;
+$heute = date("d.m.Y");	// 21.12.2016
+$netSum = 0;
 
 // print a block of text using Write()
-$this->tcPDFstart->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+// $this->tcPDFstart->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "SnapUp GmbH, Rothebühlplatz 41/1, 70178 Stuttgart", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Datum: ". $heute, '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Umsatzsteueridentifikationsnummer: F123581321", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Rechnungsnummer: ". rand(), '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Positions", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Name;\t Net;\t Tax;\t gross", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "", '', 0, 'L', true, 0, false, false, 0);
+
+foreach ($positions as &$value) {
+	$this->tcPDFstart->Write(0, $value[0] .";\t". $value[1]. ";\t". $value[1]*0.19 .";\t". $value[1]*1.19, '', 0, 'L', true, 0, false, false, 0);
+	$netSum += $value[1];
+}
+
+$this->tcPDFstart->Write(0, "", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Sum net: ". $netSum ."\t Sum Gross:". $netSum*1.19 , '', 0, 'L', true, 0, false, false, 0);
+
+$this->tcPDFstart->Write(0, "", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Bank Details", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "Bank Name: LBBW", '', 0, 'L', true, 0, false, false, 0);
+$this->tcPDFstart->Write(0, "IBAN: DE12 3456 7890 1245 6789 01", '', 0, 'L', true, 0, false, false, 0);
 
 // ---------------------------------------------------------
 
 //Close and output PDF document
-$this->tcPDFstart->Output('example_003.pdf', 'I');
+$this->tcPDFstart->Output('bill.pdf', 'I');
 
 //============================================================+
 // END OF FILE
